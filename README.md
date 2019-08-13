dd862612c7382fcde87c0b56e1a5dab4e0ee76a7
skayred <dg.freak@gmail.com>
2019-08-13 15:22:19 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# gomoku-simple
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,189 @@
+// import "./styles.css";
+
+const emptyOffset = 0; //3;
+const defaultSize = 5;
+let currentPlayer = 1;
+let gameEnded = false;
+let board = [];
+
+const findBoundaries = () => {
+  let minX = null;
+  let minY = null;
+  let maxX = null;
+  let maxY = null;
+
+  let boardCounts = board.map(row => {
+    return row.filter(e => !!e).length;
+  });
+
+  let i = boardCounts.length;
+
+  while (i-- && boardCounts[i] === 0);
+  maxY = Math.max(i, maxY || 0);
+
+  i = 0;
+  while (!boardCounts[i] && i <= board.length) {
+    i++;
+  }
+
+  minY = Math.min(i, minY || Number.MAX_VALUE);
+
+  board.forEach((row, i) => {
+    let j = row.length;
+
+    while (j-- && !row[j]);
+    maxX = Math.max(j, maxX || 0);
+
+    j = 0;
+    while (!row[j] && j <= row.length) {
+      j++;
+    }
+
+    minX = Math.min(j, minX || Number.MAX_VALUE);
+  });
+
+  return {
+    minX,
+    minY,
+    maxX,
+    maxY
+  };
+};
+
+const initialize = () => {
+  let boundaries = findBoundaries();
+  let rowLength = board[0].length;
+
+  let rowsTopCount = Math.max(emptyOffset - boundaries.minY, 0);
+  let rowsBotCount = Math.max(
+    emptyOffset - board.length + 1 + boundaries.maxY,
+    0
+  );
+  let rowsLeftCount = Math.max(emptyOffset - boundaries.minX, 0);
+  let rowsRightCount = Math.max(
+    emptyOffset - rowLength + 1 + boundaries.maxX,
+    0
+  );
+
+  let left = Array.from({ length: rowsLeftCount }, () => null);
+  let right = Array.from({ length: rowsRightCount }, () => null);
+
+  let row = Array.from(
+    { length: rowLength + rowsLeftCount + rowsRightCount },
+    () => null
+  );
+
+  board = board.map(row => {
+    return left.concat(row.concat(right));
+  });
+
+  for (let i = 0; i < rowsTopCount; i++) {
+    board = [row].concat(board);
+  }
+
+  for (let i = 0; i < rowsBotCount; i++) {
+    board = board.concat([row]);
+  }
+};
+
+const switchPlayer = () => {
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+};
+
+const someoneWon = () => {
+  return board.some((row, i) => {
+    return row.some((_, j) => {
+      if (
+        j + 4 in board &&
+        board[i][j] === currentPlayer &&
+        board[i][j + 1] === currentPlayer &&
+        board[i][j + 2] === currentPlayer &&
+        board[i][j + 3] === currentPlayer &&
+        board[i][j + 4] === currentPlayer
+      )
+        return true;
+      else if (
+        i + 4 in board &&
+        board[i][j] === currentPlayer &&
+        board[i + 1][j] === currentPlayer &&
+        board[i + 2][j] === currentPlayer &&
+        board[i + 3][j] === currentPlayer &&
+        board[i + 4][j] === currentPlayer
+      )
+        return true;
+      else if (
+        i + 4 in board &&
+        j + 4 in board &&
+        board[i][j] === currentPlayer &&
+        board[i + 1][j + 1] === currentPlayer &&
+        board[i + 2][j + 2] === currentPlayer &&
+        board[i + 3][j + 3] === currentPlayer &&
+        board[i + 4][j + 4] === currentPlayer
+      )
+        return true;
+      else if (
+        i - 4 in board &&
+        j + 4 in board &&
+        board[i][j] === currentPlayer &&
+        board[i - 1][j + 1] === currentPlayer &&
+        board[i - 2][j + 2] === currentPlayer &&
+        board[i - 3][j + 3] === currentPlayer &&
+        board[i - 4][j + 4] === currentPlayer
+      )
+        return true;
+      return false;
+    });
+  });
+};
+
+const render = () => {
+  const container = document.getElementById("board");
+
+  container.innerHTML = "";
+  const table = document.createElement("table");
+  board.forEach((row, i) => {
+    const rowElement = document.createElement("tr");
+    table.appendChild(rowElement);
+    row.forEach((el, j) => {
+      const cell = document.createElement("td");
+      cell.onclick = () => {
+        if (!board[i][j] && !gameEnded) {
+          board[i][j] = currentPlayer;
+          initialize();
+          render();
+          if (someoneWon()) {
+            gameEnded = true;
+            alert("Player " + currentPlayer + " won!");
+          } else {
+            switchPlayer();
+          }
+        }
+      };
+      switch (el) {
+        case 1:
+          cell.innerText = "x";
+          break;
+        case 2:
+          cell.innerText = "o";
+          break;
+        default:
+          cell.innerText = " ";
+      }
+      cell.className = i + "-" + j;
+      rowElement.appendChild(cell);
+    });
+  });
+  container.appendChild(table);
+};
+
+const main = () => {
+  board = [];
+  for (let i = 0; i < defaultSize; i++) {
+    board.push(Array.from({ length: defaultSize }, () => null));
+  }
+  gameEnded = false;
+  initialize();
+  render();
+};
+
+export default main;
